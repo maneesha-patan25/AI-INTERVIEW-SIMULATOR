c3574b101c7366ac8de47a640a5d79ad715da729
Maneesha.Patan <maneesha.22mis7196@vitapstudent.ac.in>
2025-08-03 10:15:55 +0530

    Header Component Completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,46 @@
+import { useAuth } from "@clerk/clerk-react";
+import { cn } from "@/lib/utils";
+import { Container } from "./container";
+import { LogoContainer } from "./logo-container";
+import { NavigationRoutes } from "./navigation-routes";
+import { NavLink } from "react-router-dom";
+import { ProfileContainer } from "./profile-container";
+import ToggleContainer from "./toggle-container";
 
+const Header = () => {
+  const { userId } = useAuth();
 
-const header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <header className={cn("w-full border-b duration-150 transition-all ease-in-out")}>
+      <Container>
+        <div className="flex items-center gap-4 w-full">
+          {/* Logo section */}
+          <LogoContainer />
 
-export default header
+          {/* Navigation section */}
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+            {userId && (
+              <NavLink
+                to="/generate"
+                className={({ isActive }) =>
+                  cn("text-base text-neutral-600", isActive && "text-neutral-900 font-semibold")
+                }
+              >
+                Take an Interview
+              </NavLink>
+            )}
+          </nav>
+
+          {/* Profile + Toggle section */}
+          <div className="ml-auto flex items-center gap-6">
+            <ProfileContainer />
+            <ToggleContainer />
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/lib/helpers.ts
@@ -0,0 +1,19 @@
+export const MainRoutes=[
+    {
+        label:"Home",
+        href:"/",
+    },
+    {
+        label:"Contact Us",
+        href:"/contactus",
+    },
+    {
+        label:"About Us",
+        href:'/aboutus',
+        
+    },
+    {
+        label:"Services",
+        href:"/services",
+    },
+];
